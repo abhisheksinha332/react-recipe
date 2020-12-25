bfed5751039b25718fbdb2d5ee699b92299c8c6c
Abhishek Sinha <3327abhishek@gmail.com>
2020-12-25 23:38:05 +0530

    Inital framework and api fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
+
 function App() {
+
+  const APP_ID = '2adf7481';
+  const APP_KEY = '0b03c5116947f05dd8d12b93d1e6c26e';
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect( () => {
+    getRecipes();
+  },[]);
+
+// const getRecipes = async () => {
+//   const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+//   const data = await response.json;
+//   console.log(data);
+  
+
+// }
+const getRecipes = async () => {
+  const respone =await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await respone.json();
+  setRecipes(data.hits);
+  
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type = "submit">Search</button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe />
+      ))}
     </div>
   );
 }
---
+++ src/Recipe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Recipe = () => {
+    return(
+        <div>
+            <h1>Tile</h1>
+            <p>Calories</p>
+            <img src="" alt=""></img>
+        </div>
+    );
+}
+
+export default Recipe;
